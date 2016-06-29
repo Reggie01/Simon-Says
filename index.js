f06879ac3ef142c610dcd29a2e4c2b65bc77a74b
Reggie S <reggie@bgcreatives.com>
2016-06-29 00:35:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿var canvas,
+      width,
+      height;
+      
+canvas = document.getElementById( "canvas" );
+height = canvas.height;
+width = canvas.width;
+
+if( canvas.getContext ) {
+    var context = canvas.getContext( "2d" );
+} else {
+    console.log( "Your browser does not support canvas." );
+}
+
+context.fillRect( 0, 0, 100, 100)
