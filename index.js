88cb887f7f3b09871e97f57f7315b87885b3540f
Reggie S <reggie@bgcreatives.com>
2016-06-29 17:34:12 -0400

    Add more responsive event listeners
    User can change games state when clicking buttons. Created a gamestate object to track different states of the application. Added different colors for a more colorful UI presentation to user. Buttons change color when clicked by user.

--- index.js
+++ index.js
@@ -5,41 +5,101 @@ var yellowBlock = document.getElementById( "yellowBlock" );
 var blueBlock = document.getElementById( "blueBlock" );
 var startButton = document.getElementById( "startContainer" );
 var strictButton = document.getElementById( "strictContainer" );
+var onButton = document.getElementById( "on" );
+var offButton = document.getElementById( "off" );
+
+var gameState = {
+    isOn: false,
+    isStart: false,
+    isStrict: false
+}
 
 function greenBlockHandler( event ){
-   event.preventDefault();
-   console.log( "green button pressed..." );
+    event.preventDefault();
+    if( gameState.isOn && gameState.isStart ){
+        console.log( "green button pressed..." );
+    }
+     
 }
 
 function redBlockHandler( event ){
-   event.preventDefault();
-   console.log( "red button pressed..." );
-
+    event.preventDefault();
+    if( gameState.isOn && gameState.isStart ){
+        console.log( "red button pressed..." );   
+    }
 }
 
 function yellowBlockHandler( event ){
-   event.preventDefault();
-   console.log( "yellow..." );
+    event.preventDefault();
+    if( gameState.isOn && gameState.isStart ){
+        console.log( "yellow..." );
+    }
 }
 
 function blueBlockHandler( event ){
-   event.preventDefault();
-   console.log( "blue button pressed..." );
+    event.preventDefault();
+    if( gameState.isOn && gameState.isStart ){
+        console.log( "blue button pressed..." );
+    }
+}
+
+function onHandler ( event ) {
+    event.preventDefault();
+    
+    if( !gameState.isOn ){
+        gameState.isOn = true;
+        onButton.style.opacity = 1; // Green A70
+        offButton.style.opacity = .3;   // Grey 900          
+    } 
+    console.log( JSON.stringify( gameState, null, 2 ) );
+}
+
+function offHandler ( event ) {
+    event.preventDefault();
+    
+    if( gameState.isOn ){
+        gameState.isOn = false;
+        offButton.style.opacity = 1;   // Black 500 
+        onButton.style.opacity = .3;  // Green 700
+        gameState.isStart = false;
+        startButton.style.opacity = .3;
+        gameState.isStrict = false;
+        strictButton.style.opacity = .3;
+    }
+    
+    console.log( JSON.stringify( gameState, null, 2 ) );
 }
 
 function startButtonHandler ( event ) {
-   event.preventDefault();
-   console.log( "Start button ..." );
+    event.preventDefault();
+    
+    if( gameState.isOn && gameState.isStart ){
+        console.log( "resetting..." );     
+    } else if( gameState.isOn ){
+        gameState.isStart = true;
+        startButton.style.opacity = 1;
+        console.log( "Start button ..." );
+    }
+    console.log( JSON.stringify( gameState, null, 2 ) );
 }
 
 function strictButtonHandler ( event ) {
-  event.preventDefault();
-  console.log( "strict button pressed... " );
+    event.preventDefault();
+        
+    if( gameState.isOn && gameState.isStart ){ 
+        gameState.isStrict = true;
+        strictButton.style.opacity = 1;
+        console.log( "strict button pressed... " );
+    }
+    
+    console.log( JSON.stringify( gameState, null, 2 ) );
 }
 
 greenBlock.addEventListener( "click", greenBlockHandler );
 redBlock.addEventListener( "click", redBlockHandler );
 yellowBlock.addEventListener( "click", yellowBlockHandler );
 blueBlock.addEventListener( "click", blueBlockHandler );
+onButton.addEventListener( "click", onHandler );
+offButton.addEventListener( "click", offHandler );
 startButton.addEventListener( "click", startButtonHandler );
 strictButton.addEventListener( "click", strictButtonHandler );
